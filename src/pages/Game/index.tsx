5427c6bbe55bd6b729658a2122fb0f5558107a36
ijlal-ishaq <ijlalishaq2019@gmail.com>
2022-02-14 23:48:32 +0500

    changes

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -1,5 +1,12 @@
-import { FC } from "react";
+/* eslint-disable eqeqeq */
+import { FC, useEffect, useState } from "react";
 import { styled } from "@mui/material/styles";
+import { ref, onValue } from "firebase/database";
+import { database } from "../../utils/firebase";
+import { concisePlayerAddress } from "../../utils/formattingFunctions";
+import axios from "axios";
+import { baseUrl } from "../../utils/urls";
+import { useWeb3React } from "@web3-react/core";
 
 const MainDiv = styled("div")(({ theme }) => ({
   marginLeft: "auto",
@@ -47,6 +54,10 @@ const GameBoard = styled("div")(({ theme }) => ({
 const GameBlock = styled("div")(({ theme }) => ({
   height: "70px",
   width: "100%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  cursor: "pointer",
 }));
 
 const VerticalDivider = styled("div")(({ theme }) => ({
@@ -55,7 +66,7 @@ const VerticalDivider = styled("div")(({ theme }) => ({
   backgroundColor: "rgba(255,255,255,0.3)",
 }));
 
-const getHeader = (turn: Number) => {
+const getHeader = (turn: string, players: any) => {
   return (
     <>
       <Heading>
@@ -76,11 +87,11 @@ const getHeader = (turn: Number) => {
             flexDirection: "column",
             fontSize: "18px",
             margin: "0px",
-            opacity: turn === 1 ? 0.5 : 0.1,
+            opacity: turn === players["player1"] ? 0.5 : 0.1,
           }}
         >
           <div style={{ textAlign: "right" }}>Player 1</div>
-          <div> 0x23e05...384</div>
+          <div>{concisePlayerAddress(players["player1"], 7)}</div>
         </Heading>
 
         <div
@@ -98,11 +109,11 @@ const getHeader = (turn: Number) => {
             flexDirection: "column",
             fontSize: "18px",
             margin: "0px",
-            opacity: turn === 2 ? 0.5 : 0.1,
+            opacity: turn === players["player2"] ? 0.5 : 0.1,
           }}
         >
           <div style={{ textAlign: "left" }}>Player 2</div>
-          <div> 0x23e05...384</div>
+          <div>{concisePlayerAddress(players["player2"], 7)}</div>
         </Heading>
       </div>
     </>
@@ -110,9 +121,82 @@ const getHeader = (turn: Number) => {
 };
 
 const Index: FC = () => {
+  const [gameId, setGameId] = useState("");
+  const [connectionKey, setConnectionKey] = useState("");
+  const [turn, setTurn] = useState("");
+  const { account } = useWeb3React();
+  const [players, setPlayers] = useState({
+    player1: ".....",
+    player2: ".....",
+  });
+  const [gameBoard, setGameBoard] = useState({
+    "1": "-",
+    "2": "-",
+    "3": "-",
+    "4": "-",
+    "5": "-",
+    "6": "-",
+    "7": "-",
+    "8": "-",
+    "9": "-",
+  });
+
+  const makeAMove = async (position: string) => {
+    if (
+      gameId !== "" &&
+      connectionKey !== "" &&
+      position !== "" &&
+      turn == account
+    ) {
+      //@ts-ignore
+      setGameBoard((board) => ({
+        ...board,
+        [position]: players.player1 == account ? "0" : "1",
+      }));
+      await axios.get(
+        baseUrl +
+          "/nfttt/" +
+          "makeAMove/" +
+          gameId +
+          "/" +
+          connectionKey +
+          "/" +
+          position
+      );
+    }
+  };
+
+  useEffect(() => {
+    onValue(ref(database, "/gamesRoom/" + gameId), (data) => {
+      if (data.val()["player1"]) {
+        setPlayers({
+          player1: data.val()["player1"],
+          player2: data.val()["player2"],
+        });
+        setTurn(data.val()["turn"]);
+        setGameBoard({
+          "1": data.val()["board"]["1"],
+          "2": data.val()["board"]["2"],
+          "3": data.val()["board"]["3"],
+          "4": data.val()["board"]["4"],
+          "5": data.val()["board"]["5"],
+          "6": data.val()["board"]["6"],
+          "7": data.val()["board"]["7"],
+          "8": data.val()["board"]["8"],
+          "9": data.val()["board"]["9"],
+        });
+      }
+    });
+  }, [gameId]);
+
+  useEffect(() => {
+    setGameId(window.location.href.split("/")[4]);
+    setConnectionKey(window.location.href.split("/")[5]);
+  }, []);
+
   return (
     <MainDiv>
-      {getHeader(1)}
+      {getHeader(turn, players)}
       <GameBoard>
         <div
           style={{
@@ -122,11 +206,29 @@ const Index: FC = () => {
             borderBottom: "solid 2px rgba(255,255,255,0.3)",
           }}
         >
-          <GameBlock />
+          <GameBlock
+            onClick={() => {
+              makeAMove("1");
+            }}
+          >
+            {gameBoard[1] == "0" ? "O" : gameBoard[1] == "1" ? "X" : null}
+          </GameBlock>
           <VerticalDivider />
-          <GameBlock />
+          <GameBlock
+            onClick={() => {
+              makeAMove("2");
+            }}
+          >
+            {gameBoard[2] == "0" ? "O" : gameBoard[2] == "1" ? "X" : null}
+          </GameBlock>
           <VerticalDivider />
-          <GameBlock />
+          <GameBlock
+            onClick={() => {
+              makeAMove("3");
+            }}
+          >
+            {gameBoard[3] == "0" ? "O" : gameBoard[3] == "1" ? "X" : null}
+          </GameBlock>
         </div>
         <div
           style={{
@@ -136,11 +238,29 @@ const Index: FC = () => {
             borderBottom: "solid 3px rgba(255,255,255,0.3)",
           }}
         >
-          <GameBlock />
+          <GameBlock
+            onClick={() => {
+              makeAMove("4");
+            }}
+          >
+            {gameBoard[4] == "0" ? "O" : gameBoard[4] == "1" ? "X" : null}
+          </GameBlock>
           <VerticalDivider />
-          <GameBlock />
+          <GameBlock
+            onClick={() => {
+              makeAMove("5");
+            }}
+          >
+            {gameBoard[5] == "0" ? "O" : gameBoard[5] == "1" ? "X" : null}
+          </GameBlock>
           <VerticalDivider />
-          <GameBlock />
+          <GameBlock
+            onClick={() => {
+              makeAMove("6");
+            }}
+          >
+            {gameBoard[6] == "0" ? "O" : gameBoard[6] == "1" ? "X" : null}
+          </GameBlock>
         </div>
         <div
           style={{
@@ -149,11 +269,29 @@ const Index: FC = () => {
             justifyContent: "space-between",
           }}
         >
-          <GameBlock />
+          <GameBlock
+            onClick={() => {
+              makeAMove("7");
+            }}
+          >
+            {gameBoard[7] == "0" ? "O" : gameBoard[7] == "1" ? "X" : null}
+          </GameBlock>
           <VerticalDivider />
-          <GameBlock />
+          <GameBlock
+            onClick={() => {
+              makeAMove("8");
+            }}
+          >
+            {gameBoard[8] == "0" ? "O" : gameBoard[8] == "1" ? "X" : null}
+          </GameBlock>
           <VerticalDivider />
-          <GameBlock />
+          <GameBlock
+            onClick={() => {
+              makeAMove("9");
+            }}
+          >
+            {gameBoard[9] == "0" ? "O" : gameBoard[9] == "1" ? "X" : null}
+          </GameBlock>
         </div>
       </GameBoard>
     </MainDiv>
